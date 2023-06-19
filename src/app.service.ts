c52d0a274579d5c0bdd0c361bda9285af93e4917
em-vivek <vivek.yadav@eminds.ai>
2023-06-19 16:00:04 +0530

    Added Overpass api for OSM response

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import fetch from 'node-fetch';
+
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getOsmData() {
+    try {
+      const response = await fetch(
+        'https://overpass-api.de/api/interpreter?'
+        , {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json;charset=UTF-8',
+          },
+          body: "[out:json];node(48.868221, 2.224983,48.868672, 2.456898)[amenity=restaurant];out;"
+        })
+      const data = await response.text()
+      console.log(data)
+      return data
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
