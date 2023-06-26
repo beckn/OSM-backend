28c763a9709ab2e1b9cd409fce51419bfb9ef9c3
em-vivek <vivek.yadav@eminds.ai>
2023-06-26 15:18:30 +0530

    Added static response

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import fetch from 'node-fetch';
+import response from './response.json'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -8,21 +9,21 @@ dotenv.config()
 export class AppService {
   async fetchStores(reqParam) {
     if (!reqParam) {
-      return {}
+      return response
     }
     try {
-      const response = await fetch(
-        process.env.OVERPASSURL
-        , {
-          method: 'POST',
-          headers: {
-            'content-type': 'application/json;charset=UTF-8',
-          },
-          body: `[out:json];node(around:5000.00,${reqParam.latitude},${reqParam.longitude})[${reqParam.tagName}=${reqParam.tagValue}];out 100;`
-        })
-      const data = await response.text()
-      let res = JSON.parse(data)
-      return res.elements
+      // const response = await fetch(
+      //   process.env.OVERPASSURL
+      //   , {
+      //     method: 'POST',
+      //     headers: {
+      //       'content-type': 'application/json;charset=UTF-8',
+      //     },
+      //     body: `[out:json];node(around:5000.00,${reqParam.latitude},${reqParam.longitude})[${reqParam.tagName}=${reqParam.tagValue}];out 100;`
+      //   })
+      // const data = await response.text()
+      // let res = JSON.parse(data)
+      return response
     } catch (error) {
       console.error(error);
     }
